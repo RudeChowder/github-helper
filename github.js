8943b4bc93f42d95c45c4cafe1200463f009fbd4
Stephen Crane <scrane88@gmail.com>
2022-11-08 14:33:59 -0500

    Update to new url and add icon

--- github.js
+++ github.js
@@ -1,9 +1,9 @@
 const createTabs = () => {
   const prNumber = window.location.href.match(/\d+/)
 
-  const buildCount = document.querySelectorAll(".commit-build-statuses").length
+  // const buildCount = document.querySelectorAll(".commit-build-statuses").length
   const ciTab = document.createElement("a")
-  ciTab.href = `https://ci.powerhrg.com/blue/organizations/jenkins/Nitro/detail/PR-${prNumber}/${buildCount}/pipeline`
+  ciTab.href = `https://ci.powerapp.cloud/job/PowerHome-Org/job/nitro-web/job/PR-${prNumber}/`
   ciTab.innerText = "CI"
   ciTab.className = "tabnav-tab flex-shrink-0 pl-5 ml-5 "
   ciTab.target = "_blank"
